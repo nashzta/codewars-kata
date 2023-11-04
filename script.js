92861410d6ba14827152a99028f8957867428d64
Jean Pierre Huaman Gomez <jeanpier.dev@outlook.com>
2023-11-04 10:30:12 -0500

    04/11/2023

--- script.js
+++ script.js
@@ -12,15 +12,17 @@
   return mainList;
 }
 
-// console.log(arrayDiff([1, 2], [1]))
-// console.log(arrayDiff([1,2,2,2,3],[2]))
-console.log(arrayDiff([1, 2, 2, 2, 3], [2, 3]));
+
 
 //*- BEST SOLUTION 
 
 function array_diff(a, b) {
     return a.filter(e => !b.includes(e));
 } */
+
+// console.log(arrayDiff([1, 2], [1]))
+// console.log(arrayDiff([1,2,2,2,3],[2]))
+// console.log(arrayDiff([1, 2, 2, 2, 3], [2, 3]));
 
 //* KATA 2
 
@@ -45,8 +47,42 @@ function array_diff(a, b) {
 // var uniqueInOrder = function (iterable) {
 //   return [...iterable].filter((a, i) => a !== iterable[i - 1]);
 // };
+
 // console.log(uniqueInOrder("AAAABBBCCDAABBB"));
 // console.log(uniqueInOrder("ABBCcAD"));
 // console.log(uniqueInOrder([1, 2, 2, 3, 3]));
 
 //* KATA 3
+
+// function order(words) {
+//   if (!words.length) return "";
+
+//   const splitWords = words.split(" ");
+
+//   const wordsInOrder = [];
+
+//   for (let x = 0; x <= splitWords.length; x++) {
+//     for (let i = 0; i < splitWords.length; i++) {
+//       if (splitWords[i].includes(`${x}`)) {
+//         wordsInOrder.push(splitWords[i]);
+//       }
+//     }
+//   }
+
+//   return wordsInOrder.join(" ");
+// }
+
+//*- Best Solution
+
+// function order(words) {
+//   return words
+//     .split(" ")
+//     .sort(function (a, b) {
+//       return a.match(/\d/) - b.match(/\d/);
+//     })
+//     .join(" ");
+// }
+
+// order("is2 Thi1s T4est 3a");
+// order("4of Fo1r pe6ople g3ood th5e the2");
+// order("");
